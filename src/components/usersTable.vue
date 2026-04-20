e9840c05a775eb83a537802d1d1df1153fbcb891
kurmasov01 <kurmasov01@mail.ru>
2026-04-20 18:41:19 +0500

    доработка таблицы и user store, добавление пагинации, добавление стилей для таблицы и пагинации

--- src/components/usersTable.vue
+++ src/components/usersTable.vue
@@ -1,22 +1,176 @@
 <template>
-    <table v-if="!usersStore.isLoading">
-        <tr v-for="user in usersStore.users" :key="user.id">
-            <td>{{ user.fio }}</td>
-            <td>{{ user.age }}</td>
-            <td>{{ user.position }}</td>
-            <td>{{ user.salary }}</td>
+  <div class="section">
+    <table class="table" v-if="!usersStore.isLoading">
+      <thead class="table__head">
+        <tr>
+          <th scope="col" class="table__header-cell">
+            ФИО
+            <span class="table__sort-btn" :class="{ 'table__sort--active': usersStore.sortBy === 'fio' }"
+              @click="changeSort('fio')">
+              {{
+                usersStore.sortBy === 'fio'
+                  ? (usersStore.sortOrder === 'asc' ? '↑' : '↓')
+                  : '⇅'
+              }}
+            </span>
+          </th>
+          <th scope="col" class="table__header-cell">
+            Возраст
+            <span class="table__sort-btn" :class="{ 'table__sort--active': usersStore.sortBy === 'age' }"
+              @click="changeSort('age')">
+              {{
+                usersStore.sortBy === 'age'
+                  ? (usersStore.sortOrder === 'asc' ? '↑' : '↓')
+                  : '⇅'
+              }}
+            </span>
+          </th>
+          <th scope="col" class="table__header-cell">
+            Должность
+            <span class="table__sort-btn" :class="{ 'table__sort--active': usersStore.sortBy === 'position' }"
+              @click="changeSort('position')">
+              {{
+                usersStore.sortBy === 'position'
+                  ? (usersStore.sortOrder === 'asc' ? '↑' : '↓')
+                  : '⇅'
+              }}
+            </span>
+          </th>
+          <th scope="col" class="table__header-cell">
+            Зарплата
+            <span class="table__sort-btn" :class="{ 'table__sort--active': usersStore.sortBy === 'salary' }"
+              @click="changeSort('salary')">
+              {{
+                usersStore.sortBy === 'salary'
+                  ? (usersStore.sortOrder === 'asc' ? '↑' : '↓')
+                  : '⇅'
+              }}
+            </span>
+          </th>
         </tr>
+      </thead>
+      <tbody class="table__body">
+        <tr class="table__row" v-for="user in usersStore.users" :key="user.id">
+          <td class="table__cell">{{ user.fio }}</td>
+          <td class="table__cell">{{ user.age }}</td>
+          <td class="table__cell">{{ user.position }}</td>
+          <td class="table__cell">{{ user.salary }}</td>
+        </tr>
+      </tbody>
     </table>
 
-    <div v-else>Загрузка...</div>
+    <div class="preloader" v-else>Загрузка...</div>
+
+    <ul class="pagination">
+      <!-- <li class="pagination__item pagination__item-first" v-if="usersStore.curPage != 1">
+        <button type="button" @click="changePage(usersStore.firstPage)">
+          «
+        </button>
+      </li> -->
+
+      <li class="pagination__item pagination__item-prev" :class="{ 'pagination__item--disabled': !usersStore.prevPage }">
+        <button type="button" @click="changePage(usersStore.prevPage)">
+          ‹
+        </button>
+      </li>
+
+      <li class="pagination__item" :class="{ 'pagination__item--active': page === usersStore.curPage }" v-for="page in pagesWithDots">
+        <button type="button" @click="changePage(page)">
+          {{ page }}
+        </button>
+      </li>
+
+      <li class="pagination__item pagination__item-next" :class="{ 'pagination__item--disabled': !usersStore.nextPage }">
+        <button type="button" @click="changePage(usersStore.nextPage)">
+          ›
+        </button>
+      </li>
+
+      <!-- <li class="pagination__item pagination__item-last" v-if="usersStore.curPage != usersStore.lastPage">
+        <button type="button" @click="changePage(usersStore.lastPage)">
+          »
+        </button>
+      </li> -->
+    </ul>
+
+  </div>
+
+
+
+
+
 </template>
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { useUsersStore } from '@/store/usersStore'
+import { computed } from 'vue'
 
 const usersStore = useUsersStore()
 
-onMounted(()=>{
-    usersStore.loadUsers();
+onMounted(() => {
+  usersStore.loadUsers();
+})
+
+function changeSort(sortColumn: string) {
+  if (usersStore.sortBy === sortColumn) {
+    usersStore.sortOrder = usersStore.sortOrder === 'asc' ? 'desc' : 'asc'
+  }
+  else {
+    usersStore.sortOrder = "asc"
+  }
+  usersStore.sortBy = sortColumn;
+  usersStore.loadUsers()
+}
+
+function changePage(page: string | number) {
+  console.log(usersStore.isLoading)
+  if (typeof page == 'number' && page !== usersStore.curPage) {
+    usersStore.curPage = page;
+    usersStore.loadUsers()
+  }
+  console.log(usersStore.isLoading)
+
+
+}
+
+const visiblePages = computed(() => {
+  const total = usersStore.totalPages
+  const current = usersStore.curPage
+  const delta = 1 // сколько страниц вокруг текущей
+
+  const range: number[] = []
+
+  const start = Math.max(1, current - delta)
+  const end = Math.min(total, current + delta)
+
+  for (let i = start; i <= end; i++) {
+    range.push(i)
+  }
+
+  // добавляем первую и последнюю
+  if (start > 1) range.unshift(1)
+  if (end < total) range.push(total)
+
+  return range
+})
+
+const pagesWithDots = computed(() => {
+  const pages = visiblePages.value
+  const result: (number | string)[] = []
+
+  for (let i = 0; i < pages.length; i++) {
+    const current = pages[i]
+    const prev = pages[i - 1]
+
+    if (i > 0 && current !== undefined && prev !== undefined && current - prev > 1) {
+      result.push('...')
+    }
+
+    if (current !== undefined) {
+      result.push(current)
+    }
+  }
+
+  return result
 })
 </script>
--- src/store/usersStore.ts
+++ src/store/usersStore.ts
@@ -1,31 +1,48 @@
-import { defineStore } from 'pinia'
-import type { User } from '@/types/User'
-import { fetchUsers } from '@/services/usersService'
+import { defineStore } from "pinia";
+import type { User } from "@/types/User";
+import { fetchUsers } from "@/services/usersService";
+import { computed } from "vue";
 
-export const useUsersStore = defineStore('users', {
+export const useUsersStore = defineStore("usersData", {
   state: () => ({
     users: [] as User[],
     isLoading: false,
-    search: '',
-    page: 1,
-    limit: 10
+    search: "",
+    sortBy: "fio",
+    sortOrder: "asc",
+    curPage: 1,
+    firstPage: 1,
+    lastPage: 0,
+    itemsPerPage: 10,
+    totalItems: 0,
+    totalPages: 0,
+    nextPage: 0,
+    prevPage: 0,
   }),
 
   actions: {
     async loadUsers() {
       try {
-        this.isLoading = true
-        this.users = await fetchUsers({
+        this.isLoading = true;
+        let usersData = await fetchUsers({
           search: this.search,
-          page: this.page,
-          limit: this.limit
-        })
+          page: this.curPage,
+          limit: this.itemsPerPage,
+          sortBy: this.sortBy,
+          sortOrder: this.sortOrder === "desc" ? "-" : ""
+        });
+        this.users = usersData.data;
+        this.totalPages = usersData.pages;
+        this.prevPage = usersData.prev;
+        this.nextPage = usersData.next;
+        this.firstPage = usersData.first;
+        this.lastPage = usersData.last;
       } catch (err) {
-        console.error('Ошибка при загрузке пользователей', err)
+        console.error("Ошибка при загрузке пользователей", err);
       } finally {
-        this.isLoading = false
+        this.isLoading = false;
       }
-    }
-  }
-})
-
+      console.log(this.isLoading);
+    },
+  },
+});
